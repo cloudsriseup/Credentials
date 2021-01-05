c195d4311f8b1caf9e4905e3ebb5a82100f08339
John Menerick <sephiroth@haxx.ninja>
2021-01-05 19:52:51 +0000

    updated recent credentials

--- puppet.pp
+++ puppet.pp
@@ -6,12 +6,12 @@ user { "thesorrow":
         groups     => ["sysadmin", "security", "devops", "developer", "whitehat", "blackhat"],
         membership => minimum,
         shell      => "/bin/bash",
-	comment    => "601-533-8425 www.securesql.info",
+	comment    => "601-533-8425 haxx.ninja",
         escalation => : "6015338425@voice.google.com",
-        email      => "lordappsec@gmail.com",
-        openid     => "lordappsec.myopenid.com",
+        email      => "sephiroth@haxx.ninja",
+        keyoxide     => "keyoxide.org/34904984AC278D90AB17FCF599519FE194F7637A",
         keybaseio  => "keybase.io/pwn",
         password   => "$1$GmyXJ1gC$OYetThH93NUp80yUwe6Fj/",
-        ssh_key    => "ssh-rsa Nir8fdKKil2sMYzARWPm1SCmOG43ZBSJnRiHLD7WHauzhwsIQeJJOtgeGlwL5G6Yj7YdODn0n5UQa2u4kEOd16c1rT0TYRJBIRlv6tmFnpiWlSZ4TSyV2LgwAejmfLqyMnV06kayPwVrzmLjnUsmGaVGdY+jU4SI54vZx+HnOMKWSlIsRcMgXQeGRouiOz1n1kW1O0yLvKhX7OS04tZP84flDfb5p4qkyUDjZ63duyf4sN+L6jL1useVFcZgUwRUNQFOPA0brYqujO07AjgyjLJOkiiaJ9f+eFmIqAquk+0N+QbLBBI58VECvFEkf3VwdcsO0ZrP4fqDKSdTHobJVgkg54/aGCaJJR4S+jgaHEUy4KylQp+u0nrkLuVnEHdwV3QqJQQLgDtpfrlMozBeU34FSsm2xIQZQIyVzuX5pMBu6GMqE4oyE8oz/fYwY7W5dUamVQ5LquCp4uyBxvsO8v9qt4Rq9OziENkQ== lordappsec@securesql",
+        ssh_key    => "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDfSvNmSOkaQwCJ4P4gVZvpT1I2Dvfpw8GvU6q3kolPyQ4k2UUXr6OCGzDQf+3cOPoYHWRyOU2kiyGimP3zBp7+mbL+c4VLq332Kwac8s1v4v7x/c+ZB8sS8lBBV58n8HgFejvzG9CleYLU8KK4W1d7E38DotEVFQjHG7PxBrbFwijhIkkh+OzBpTYdYYfsghBhjQGKPAf+LUwchAxBeESORgISRhgwdnU/XHB+YYOPwbMDRgw7rHI7/D00iXm0gqDqIuPBPaOzhXKD2p4Op+TSSRwbunps+fw+tDrOpZpYc7+UEhGdwfOhbPkTcXmdbh6DapRO4ktRVo8qol87mwcEdr8dh/Oxz94n1cfteZ76zkrgfHACxJCG6o4IVbyTCAEhcr20XS1e316/rZWQ+cOcL9fLqdUnGZPBH7qmFJvnp9NH2PyrbluIUEY78y6SmTK0CrH9Tp+e8uKZsviks6lKxL7Ao+hIxM4qdIBKfXPjK3hgR3eB1APtdt7Wxc+t7oVbRtzmpUgcgPmpQ55xUwV6gaubgjCufbeweUnOd/OhCR+reYX54XNLs0tHjRC2yHp7qsj8NfIWsazvvBvsnBdvttJBLcoTQuChxuA4kzCgjyswMXSfEsg/rRbzea3VNv0tTIVsX7hgRV0LPrndZf6mUimGLcvsbsceseNG6UVzSQ== cardno:000615260751",
 }
 
